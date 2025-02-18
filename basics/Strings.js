f1ab7e9ad8a11c46dc5027f0cdad5e1eac932a22
Kaushio <154815342+Kaushio@users.noreply.github.com>
2025-02-18 04:15:43 +0000

    newlearning

---
+++ basics/Strings.js
@@ -0,0 +1,18 @@
+// let str1 = "hello"
+// let str2 = " sir!!"
+// let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+1+"2")
+// console.log("1"+2+2)
+
+console.log(null>0)
+console.log(null==0)
+console.log(null>=0)
+ 
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0 )
+// we avoid these kind of comparison   
---
+++ basics/variable.js
@@ -0,0 +1,8 @@
+const AccountId = 123945
+let Accountemail =  "kaushio@gmail.com"
+var Accountpassword = "12345"
+AccountHolderName =  "kaushik sengupta"
+
+// AccountId =  2 //not allowed
+
+console.log(AccountId);
